fd645e58de6a512fe24c18050788278d6dec9fe7
a241112 <af.vly-gtr.1112@outlook.jp>
2020-09-24 12:31:43 +0900

    カラムの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :items, only: :index
-  #get 'items', to: 'items#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -27,8 +27,14 @@ ActiveRecord::Schema.define(version: 2020_09_23_084256) do
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "last_name", null: false
+    t.string "first_name", null: false
+    t.string "last_name_kana", null: false
+    t.string "first_name_kane", null: false
+    t.date "birthday", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
