64ed11ddc75f770bd2ed16719f8dcacd4ded1d29
a241112 <af.vly-gtr.1112@outlook.jp>
2020-09-25 15:01:26 +0900

    リンクの導入

--- db/schema.rb
+++ db/schema.rb
@@ -33,7 +33,7 @@ ActiveRecord::Schema.define(version: 2020_09_23_084256) do
     t.string "last_name", null: false
     t.string "first_name", null: false
     t.string "last_name_kana", null: false
-    t.string "first_name_kane", null: false
+    t.string "first_name_kana", null: false
     t.date "birthday", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
