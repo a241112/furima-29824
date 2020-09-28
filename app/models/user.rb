76eea0ce99841791b999f98e7823ff2aaccf528e
a241112 <af.vly-gtr.1112@outlook.jp>
2020-09-28 17:19:43 +0900

    テストコードの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :name, :last_name, :first_name, :last_name_kana, :first_name_kana, :birthday, presence: true
-  validates :password, length: { minimum: 6 }
+  validates :name, :birthday, presence: true
+  validates :password, confirmation: true, length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+  validates :email, uniqueness: true ,format: { with: /@/}
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ } do
+    validates :first_name
+    validates :last_name
+  end
+  with_options presence: true, format: { with: /\A[ァ-ン]+\z/ } do
+    validates :first_name_kana
+    validates :last_name_kana
+  end
 end
