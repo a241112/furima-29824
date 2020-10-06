af276c787120a06f1a3d1315acc4fbe538cba255
a241112 <af.vly-gtr.1112@outlook.jp>
2020-10-06 14:21:48 +0900

    コントローラーの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :set_item ,only: [:edit, :show]
+  before_action :set_item ,only: [:edit, :show, :update]
   before_action :authenticate_user!, only: [:new]
   def index
     @item = Item.all.order('created_at DESC')
@@ -25,7 +25,6 @@ class ItemsController < ApplicationController
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to root_path
     else
