6612ef3f2aff797b7af3394357b3e592ed9b9efb
a241112 <af.vly-gtr.1112@outlook.jp>
2020-10-08 16:25:00 +0900

    モデル名の変更

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,4 +1,4 @@
-class Transaction < ApplicationRecord
+class Purchase < ApplicationRecord
   belongs_to :user
   belongs_to :item
   has_one :address
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_07_060931) do
+ActiveRecord::Schema.define(version: 2020_10_08_072114) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -55,6 +55,13 @@ ActiveRecord::Schema.define(version: 2020_10_07_060931) do
     t.integer "shipping_day_id", null: false
     t.integer "price", null: false
     t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "purchases", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "item_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
