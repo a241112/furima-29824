b4ee4e8eb6b28334fa5a2f9b84a999e0409f7bd2
a241112 <af.vly-gtr.1112@outlook.jp>
2020-10-14 15:53:29 +0900

    formoオブジェクトの設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,16 +1,22 @@
 class OrdersController < ApplicationController
   def index
+    @order = Order.new
+    @item = Item.find(params[:item_id])
   end
-
-  def new
-  end
-
+  
   def create
+    # binding.pry
     @order = Order.new(order_params)
-    if 
-    @address = Address.create(address_params)
+    if @order.valid?
+      pay_item
+      @order.save
+      redirect_to root_path
+    else
+      @item = Item.find(params[:item_id])
+      render :index
+    end
   end
-
+  
   private
   
   def pay_item
@@ -24,10 +30,7 @@ class OrdersController < ApplicationController
   end
 
   def order_params
-    params.merge(token: params[:token])
+    params.require(:order).permit(:post_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :purchase_id).merge(user_id: current_user.id, token: params[:token], item_id: params[:item_id])
   end
 
-  def address_params
-    params.permit(:post_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :purchase_id)
-  end
 end
---
+++ app/models/order.rb
@@ -0,0 +1,18 @@
+class Order
+  include ActiveModel::Model
+  attr_accessor :post_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :purchase_id, :item_id, :user_id, :token
+
+  with_options presence: true do
+    validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :city, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
+    validates :house_number
+    validates :phone_number, length: {maximum: 11 }, format: { with: /\A[0-9]+\z/i, message: "is invalid. Input half-width numbers."}
+    validates :token
+  end
+  validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
+
+  def save
+    purchase = Purchase.create(item_id: item_id, user_id: user_id)
+    Address.create(post_code: post_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number, purchase_id: purchase.id)
+  end
+end
