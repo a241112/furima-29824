d70dce934a7917f548256d4268f993c6a14243c3
a241112 <af.vly-gtr.1112@outlook.jp>
2020-10-15 03:13:30 +0900

    ストロングパラメーターの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -54,6 +54,6 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.permit(:name, :description, :category_id, :condition_id, :postage_id, :prefecture_id, :shipping_day_id, :price, :image, :item_id).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :description, :category_id, :condition_id, :postage_id, :prefecture_id, :shipping_day_id, :price, :image).merge(user_id: current_user.id)
   end
 end
